63fbc608193f474a72c58a29abe8cf25a1bd6243
Rahul Dasi <rahul.d@mplussoft.com>
2021-11-13 13:16:48 +0530

    Renamed package name & app name

---
+++ app/src/main/java/com/poona/agrocart/data/network/ApiClientAuth.java
@@ -0,0 +1,80 @@
+package com.poona.agrocart.data.network;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+
+import com.poona.agrocart.data.shared_preferences.AppSharedPreferences;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.poona.agrocart.app.AppConstants.AUTH_ACCESS_TOKEN;
+import static com.poona.agrocart.app.AppConstants.BASE_URL;
+
+/**
+ * Created by Rahul Dasi on 6/10/2020
+ */
+public class ApiClientAuth
+{
+    private static Retrofit retrofit = null;
+    private static OkHttpClient okHttpClient;
+    private static AppSharedPreferences appSharedPreferences;
+
+    public static Retrofit getClient(Context context)
+    {
+        appSharedPreferences = new AppSharedPreferences(context);
+
+        if(okHttpClient == null)
+            initOkHttp(context);
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    private static void initOkHttp(final Context context)
+    {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+                .callTimeout(2, TimeUnit.MINUTES)
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .addInterceptor(chain ->
+                {
+                    Request original = chain.request();
+                    Request.Builder requestBuilder = original.newBuilder()
+                            .addHeader("Accept", "application/json")
+                            .addHeader("Content-Type", "application/json");
+
+                    // Adding Authorization token (API Key)
+                    // Requests will be denied without API key
+                    if (appSharedPreferences.getAuthorizationToken() != null
+                            && !TextUtils.isEmpty(appSharedPreferences.getAuthorizationToken()))
+                    {
+                        Log.e(AUTH_ACCESS_TOKEN, appSharedPreferences.getAuthorizationToken());
+                        requestBuilder.addHeader(AUTH_ACCESS_TOKEN, appSharedPreferences.getAuthorizationToken());
+                    }
+
+                    Request request = requestBuilder.build();
+                    return chain.proceed(request);
+                });
+
+
+        okHttpClient = httpClient.build();
+    }
+}
---
+++ app/src/main/java/com/poona/agrocart/ui/BaseActivity.java
@@ -0,0 +1,272 @@
+package com.poona.agrocart.ui;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ContextThemeWrapper;
+
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.InstallStateUpdatedListener;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.InstallStatus;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.Task;
+import com.poona.agrocart.R;
+import com.poona.agrocart.data.firebase.PushNotification;
+import com.poona.agrocart.ui.splash_screen.SplashScreenActivity;
+import com.poona.agrocart.widgets.CustomButton;
+import com.poona.agrocart.widgets.CustomTextView;
+
+import static com.poona.agrocart.app.AppConstants.FROM_SCREEN;
+import static com.poona.agrocart.app.AppConstants.LOGOUT;
+
+/**
+ * Created by Rahul Dasi on 6/10/2020
+ */
+public abstract class BaseActivity extends AppCompatActivity
+{
+    private static final int REQ_CODE_VERSION_UPDATE = 530;
+    private static String TAG = BaseActivity.class.getSimpleName();
+    private AppUpdateManager appUpdateManager;
+    private InstallStateUpdatedListener installStateUpdatedListener;
+
+    protected PushNotification pushNotification;
+
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkNewAppVersionState();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, final int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        switch (requestCode) {
+
+            case REQ_CODE_VERSION_UPDATE:
+                if (resultCode != RESULT_OK) { //RESULT_OK / RESULT_CANCELED / RESULT_IN_APP_UPDATE_FAILED
+                    System.out.println("Update flow failed! Result code: " + resultCode);
+                    // If the update is cancelled or fails,
+                    // you can request to start the update again.
+                    unregisterInstallStateUpdListener();
+                }
+
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterInstallStateUpdListener();
+        super.onDestroy();
+    }
+
+    protected void checkForAppUpdate() {
+        // Creates instance of the manager.
+        appUpdateManager = AppUpdateManagerFactory.create(this);
+
+        // Returns an intent object that you use to check for an update.
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+
+        // Create a listener to track request state updates.
+        installStateUpdatedListener = installState -> {
+            // Show module progress, log state, or install the update.
+            if (installState.installStatus() == InstallStatus.DOWNLOADED)
+                // After the update is downloaded, show a notification
+                // and request user confirmation to restart the app.
+                popupForCompleteUpdateAndUnregister();
+        };
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                // Request the update.
+                if (appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
+
+                    // Before starting an update, register a listener for updates.
+                    appUpdateManager.registerListener(installStateUpdatedListener);
+                    // Start an update.
+                    startAppUpdateFlexible(appUpdateInfo);
+                } else if (appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE) ) {
+                    // Start an update.
+                    startAppUpdateImmediate(appUpdateInfo);
+                }
+            }
+        }).addOnFailureListener(Throwable::printStackTrace);
+    }
+
+    private void startAppUpdateImmediate(AppUpdateInfo appUpdateInfo) {
+        try {
+            appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    // The current activity making the update request.
+                    this,
+                    // Include a request code to later monitor this update request.
+                    BaseActivity.REQ_CODE_VERSION_UPDATE);
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startAppUpdateFlexible(AppUpdateInfo appUpdateInfo) {
+        try {
+            appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.FLEXIBLE,
+                    // The current activity making the update request.
+                    this,
+                    // Include a request code to later monitor this update request.
+                    BaseActivity.REQ_CODE_VERSION_UPDATE);
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+            unregisterInstallStateUpdListener();
+        }
+    }
+
+    /**
+     * Displays the snackbar notification and call to action.
+     * Needed only for Flexible app update
+     */
+    protected void popupForCompleteUpdateAndUnregister()
+    {
+        /*Snackbar snackbar = Snackbar.make(coordinatorLayoutMain, getString(R.string.update_downloaded), Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction(R.string.restart, (View.OnClickListener) view -> appUpdateManager.completeUpdate());
+        snackbar.setActionTextColor(ContextCompat.getColor(this, R.color.colorRed));
+        snackbar.show();*/
+
+        showUpdateSuccessPopup();
+        unregisterInstallStateUpdListener();
+    }
+
+    /**
+     * Checks that the update is not stalled during 'onResume()'.
+     * However, you should execute this check at all app entry points.
+     */
+    private void checkNewAppVersionState() {
+        appUpdateManager
+                .getAppUpdateInfo()
+                .addOnSuccessListener(
+                        appUpdateInfo -> {
+                            //FLEXIBLE:
+                            // If the update is downloaded but not installed,
+                            // notify the user to complete the update.
+                            if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                                popupForCompleteUpdateAndUnregister();
+                            }
+
+                            //IMMEDIATE:
+                            if (appUpdateInfo.updateAvailability()
+                                    == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                                // If an in-app update is already running, resume the update.
+                                startAppUpdateImmediate(appUpdateInfo);
+                            }
+                        })
+                .addOnFailureListener(Throwable::printStackTrace);
+    }
+
+    /**
+     * Needed only for FLEXIBLE update
+     */
+    private void unregisterInstallStateUpdListener() {
+        if (appUpdateManager != null && installStateUpdatedListener != null)
+            appUpdateManager.unregisterListener(installStateUpdatedListener);
+    }
+
+    private void showUpdateSuccessPopup()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.StyleDataConfirmationDialog));
+
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_logout,null);
+
+        builder.setView(dialogView);
+        builder.setCancelable(false);
+
+        CustomTextView
+                tvHeading = dialogView.findViewById(R.id.tv_heading);
+        CustomButton customButtonYes = dialogView.findViewById(R.id.btn_yes);
+        CustomButton customButtonNo = dialogView.findViewById(R.id.btn_no);
+
+        tvHeading.setText(R.string.update_downloaded);
+        customButtonYes.setText(R.string.restart);
+
+        customButtonNo.setVisibility(View.GONE);
+
+        final AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.StyleDialogUpDownAnimation;
+
+        customButtonYes.setOnClickListener(v -> {
+            dialog.dismiss();
+            appUpdateManager.completeUpdate();
+            //doRestart();
+        });
+
+        customButtonNo.setOnClickListener(v -> dialog.dismiss());
+
+        dialog.show();
+
+        // Get screen width and height in pixels
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        // The absolute width of the available display size in pixels.
+        int displayWidth = displayMetrics.widthPixels;
+        // The absolute height of the available display size in pixels.
+        int displayHeight = displayMetrics.heightPixels;
+
+        //int displayWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+        //int displayHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+
+        // Initialize a new window manager layout parameters
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+
+        // Copy the alert dialog window attributes to new layout parameter instance
+        layoutParams.copyFrom(dialog.getWindow().getAttributes());
+
+        // Set the alert dialog window width and height
+        // Set alert dialog width equal to screen width 90%
+        // int dialogWindowWidth = (int) (displayWidth * 0.9f);
+        // Set alert dialog height equal to screen height 90%
+        // int dialogWindowHeight = (int) (displayHeight * 0.9f);
+
+        // Set alert dialog width equal to screen width 100%
+        int dialogWindowWidth = (int) (displayWidth * 0.8f);
+        // Set alert dialog height equal to screen height 100%
+        //int dialogWindowHeight = (int) (displayHeight * 0.9f);
+
+        // Set the width and height for the layout parameters
+        // This will bet the width and height of alert dialog
+        layoutParams.width = dialogWindowWidth;
+        //layoutParams.height = dialogWindowHeight;
+
+        // Apply the newly created layout parameters to the alert dialog window
+        dialog.getWindow().setAttributes(layoutParams);
+    }
+
+    private void doRestart() {
+        Intent intent = new Intent(this, SplashScreenActivity.class);
+        intent.putExtra(FROM_SCREEN, LOGOUT);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        this.finish();
+    }
+}
