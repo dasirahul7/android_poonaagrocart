012d289b20a11b8de6a392ef44285458266df2f6
ranju <ranju.g@mplussoft.in>
2021-11-18 19:23:53 +0530

    DashBoardActivity just created need to add navigation

--- app/src/main/java/com/poona/agrocart/ui/login/LogInFragment.java
+++ app/src/main/java/com/poona/agrocart/ui/login/LogInFragment.java
@@ -1,17 +1,22 @@
 package com.poona.agrocart.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavOptions;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.poona.agrocart.R;
 import com.poona.agrocart.ui.BaseFragment;
+import com.poona.agrocart.ui.dashboard.DashBoardActivity;
+import com.poona.agrocart.ui.intro.IntroScreenFragment;
 import com.poona.agrocart.ui.splash_screen.OnBackPressedListener;
 
 /**
@@ -23,6 +28,13 @@ public class LogInFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(requireActivity(), DashBoardActivity.class);
+                startActivity(intent);
+            }
+        },1000);
         return inflater.inflate(R.layout.fragment_log_in, container, false);
     }
 }
