0cadd7a61c43d51543f6f37e84520f10164e40f5
ranju <ranju.g@mplussoft.in>
2021-12-11 19:36:12 +0530

    -Sign Up Radio done

--- app/src/main/java/com/poona/agrocart/ui/sign_up/SignUpFragment.java
+++ app/src/main/java/com/poona/agrocart/ui/sign_up/SignUpFragment.java
@@ -1,23 +1,27 @@
 package com.poona.agrocart.ui.sign_up;
 
 import static com.poona.agrocart.ui.splash_screen.SplashScreenActivity.ivBack;
+
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import com.hbb20.CountryCodePicker;
 import com.poona.agrocart.R;
 import com.poona.agrocart.app.AppConstants;
@@ -25,10 +29,10 @@ import com.poona.agrocart.databinding.FragmentSignUpBinding;
 import com.poona.agrocart.ui.BaseFragment;
 import com.poona.agrocart.ui.login.BasicDetails;
 import com.poona.agrocart.ui.login.CommonViewModel;
+
 import java.util.Objects;
 
-public class SignUpFragment extends BaseFragment implements View.OnClickListener
-{
+public class SignUpFragment extends BaseFragment implements View.OnClickListener {
     private FragmentSignUpBinding fragmentSignUpBinding;
     private CommonViewModel commonViewModel;
     private BasicDetails basicDetails;
@@ -40,7 +44,7 @@ public class SignUpFragment extends BaseFragment implements View.OnClickListener
         fragmentSignUpBinding.setLifecycleOwner(this);
         final View view = ((ViewDataBinding) fragmentSignUpBinding).getRoot();
 
-        commonViewModel=new ViewModelProvider(this).get(CommonViewModel.class);
+        commonViewModel = new ViewModelProvider(this).get(CommonViewModel.class);
         fragmentSignUpBinding.setCommonViewModel(commonViewModel);
 
         initView(view);
@@ -50,8 +54,7 @@ public class SignUpFragment extends BaseFragment implements View.OnClickListener
         return view;
     }
 
-    private void initView(View view)
-    {
+    private void initView(View view) {
         fragmentSignUpBinding.tvTermsOfService.setOnClickListener(this);
         fragmentSignUpBinding.tvPrivacyPolicy.setOnClickListener(this);
         fragmentSignUpBinding.btnSignUp.setOnClickListener(this);
@@ -64,13 +67,12 @@ public class SignUpFragment extends BaseFragment implements View.OnClickListener
 
         hideKeyBoard(requireActivity());
         Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).hide();
-        basicDetails=new BasicDetails();
+        basicDetails = new BasicDetails();
 
-        Bundle bundle=this.getArguments();
-        if(bundle!=null)
-        {
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
             fragmentSignUpBinding.etPhoneNo.setText(bundle.getString(AppConstants.MOBILE_NO));
-            String tempCountryCode=bundle.getString(AppConstants.COUNTRY_CODE).replace("+","");
+            String tempCountryCode = bundle.getString(AppConstants.COUNTRY_CODE).replace("+", "");
             fragmentSignUpBinding.countryCodePicker.setCountryForPhoneCode(Integer.parseInt(tempCountryCode));
 
             basicDetails.setCountryCode(fragmentSignUpBinding.countryCodePicker.getSelectedCountryCodeWithPlus());
@@ -87,20 +89,23 @@ public class SignUpFragment extends BaseFragment implements View.OnClickListener
         setUpTextWatcher();
     }
 
-    public void setUpCountryCodePicker()
-    {
-        Typeface typeFace=Typeface.createFromAsset(requireContext().getAssets(),getString(R.string.font_poppins_regular));
+    public void setUpCountryCodePicker() {
+        Typeface typeFace = Typeface.createFromAsset(requireContext().getAssets(), getString(R.string.font_poppins_regular));
         fragmentSignUpBinding.countryCodePicker.setTypeFace(typeFace);
         fragmentSignUpBinding.countryCodePicker.setDialogEventsListener(new CountryCodePicker.DialogEventsListener() {
             @Override
             public void onCcpDialogOpen(Dialog dialog) {
-                TextView title=dialog.findViewById(R.id.textView_title);
+                TextView title = dialog.findViewById(R.id.textView_title);
                 title.setText(getString(R.string.select_country));
             }
+
             @Override
-            public void onCcpDialogDismiss(DialogInterface dialogInterface) { }
+            public void onCcpDialogDismiss(DialogInterface dialogInterface) {
+            }
+
             @Override
-            public void onCcpDialogCancel(DialogInterface dialogInterface) { }
+            public void onCcpDialogCancel(DialogInterface dialogInterface) {
+            }
         });
 
         /*fragmentSignUpBinding.countryCodePicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
@@ -111,17 +116,19 @@ public class SignUpFragment extends BaseFragment implements View.OnClickListener
         });*/
     }
 
-    private void setUpTextWatcher()
-    {
+    private void setUpTextWatcher() {
         fragmentSignUpBinding.etPhoneNo.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length()==10)
-                {
+                if (s.length() == 10) {
                     hideKeyBoard(requireActivity());
                 }
             }
@@ -131,12 +138,13 @@ public class SignUpFragment extends BaseFragment implements View.OnClickListener
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
-        switch (v.getId())
-        {
+        switch (v.getId()) {
             case R.id.tv_terms_of_service:
+                redirectToTermsAndCondtn(v);
                 break;
 
             case R.id.tv_privacy_policy:
+                redirectToPrivacyPolicy(v);
                 break;
 
             case R.id.btn_sign_up:
@@ -154,51 +162,57 @@ public class SignUpFragment extends BaseFragment implements View.OnClickListener
 
     }
 
-    private void changeBgs(boolean isIndividualSelected)
-    {
-        if(isIndividualSelected)
-        {
+    private void changeBgs(boolean isIndividualSelected) {
+        if (isIndividualSelected) {
             fragmentSignUpBinding.rbIndividual.setBackgroundResource(R.drawable.bg_signup_rb_selected);
             fragmentSignUpBinding.rbBusiness.setBackgroundResource(R.drawable.bg_signup_rb_unselected);
-        }
-        else
-        {
+        } else {
             fragmentSignUpBinding.rbIndividual.setBackgroundResource(R.drawable.bg_signup_rb_unselected);
             fragmentSignUpBinding.rbBusiness.setBackgroundResource(R.drawable.bg_signup_rb_selected);
         }
     }
 
-    private void RedirectToSelectLocationFragment(View v)
-    {
+    private void RedirectToSelectLocationFragment(View v) {
         basicDetails.setUserName(fragmentSignUpBinding.etUsername.getText().toString());
         basicDetails.setCountryCode(fragmentSignUpBinding.countryCodePicker.getSelectedCountryCodeWithPlus());
         basicDetails.setMobileNumber(fragmentSignUpBinding.etPhoneNo.getText().toString());
         basicDetails.setEmailId(fragmentSignUpBinding.etEmailId.getText().toString());
 
-        int errorCodeUserName=basicDetails.isValidUserName();
-        int errorCodeEmailId=basicDetails.isValidEmailId();
+        int errorCodeUserName = basicDetails.isValidUserName();
+        int errorCodeEmailId = basicDetails.isValidEmailId();
 
-        if(errorCodeUserName==0){
-            errorToast(requireActivity(),getString(R.string.username_should_not_be_empty));
-        }
-        else if(errorCodeEmailId==1){
-            errorToast(requireActivity(),getString(R.string.please_enter_valid_email_id));
-        }
-        else {
+        if (errorCodeUserName == 0) {
+            errorToast(requireActivity(), getString(R.string.username_should_not_be_empty));
+        } else if (errorCodeEmailId == 1) {
+            errorToast(requireActivity(), getString(R.string.please_enter_valid_email_id));
+        } else {
             if (isConnectingToInternet(context)) {
                 //add API call here
                 commonViewModel.mobileNo.setValue(basicDetails.getMobileNumber());
                 commonViewModel.countryCode.setValue(basicDetails.getCountryCode());
                 commonViewModel.userName.setValue(basicDetails.getUserName());
                 commonViewModel.emailId.setValue(basicDetails.getEmailId());
-                successToast(context,"Login Success");
+                successToast(context, "Login Success");
                 Navigation.findNavController(v).navigate(R.id.action_signUpFragment_to_selectLocationFragment);
 
-            }
-            else {
+            } else {
                 showNotifyAlert(requireActivity(), context.getString(R.string.info), context.getString(R.string.internet_error_message), R.drawable.ic_no_internet);
             }
         }
 
+    }
+
+    private void redirectToPrivacyPolicy(View v) {
+        Bundle bundle = new Bundle();
+        bundle.putString("from", "SignUp");
+        bundle.putString("title", getString(R.string.privacy_policy));
+        Navigation.findNavController(v).navigate(R.id.action_signUpFragment_to_signInPrivacyFragment, bundle);
+    }
+
+    private void redirectToTermsAndCondtn(View v) {
+        Bundle bundle = new Bundle();
+        bundle.putString("from", "SignUp");
+        bundle.putString("title", getString(R.string.menu_terms_conditions));
+        Navigation.findNavController(v).navigate(R.id.action_signUpFragment_to_signInTermsFragment, bundle);
     }
 }
